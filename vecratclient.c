1b5076e0b4e66350901f3154eab993d4ad7a627e
Leelalu <leelalu821@gmail.com>
2021-01-25 23:47:56 -0700

    Implementation(incomplete)

--- vecratclient.c
+++ vecratclient.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <string.h>
 
-#define SHAREDFILE "/.vecratsharedfile"
+#define SHMKEY 893422
+#define SHMSIZE 8
+#define INTSIZE 4
 
-int *arrayPntrOffset[2];
-char *sharedFileDir;
-int fileDescript;
-int *mmapFileDescript;
+int shmId;
+int *shmPntr;
 
 int main(int argc, char **argv){
-  // Create set of integers to write to mem file
-  arrayPntrOffset[1] = atoi(argv[1]);
-  arrayPntrOffset[2] = atoi(argv[2]);
+  // Map pointer to memory w/ SHMKEY
+  if((shmId=shmget(SHMKEY, SHMSIZE, IPC_CREAT | 0770)) < 0){
+    printf("Unable to get memory space...\n");
+    perror("shmget");
+    return(0);
+  }
+  else if((shmPntr=shmat(shmId, NULL, IPC_CREAT | 0770)) < 0){
+    printf("Unable to get memory space...\n");
+    perror("shmmat");
+    return(0);
+  }
 
-  // Get shared file directory
-  sharedFileDir = strcat(getenv("HOME"), SHAREDFILE);
-
-  // Create a file descriptor and open file
-
-  // Map file to memory
-
-  // Write args to file
-
-  // Close Pointers/file
-  close(fileDescript);
-  munmap(mmapFileDescript, 20);
+  // Write args to var
+  shmPntr[0]=atoi(argv[1]);
+  shmPntr[1]=atoi(argv[2]);
 
   // Exit
   return(0);
--- vecratserver.c
+++ vecratserver.c
@@ -1,60 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
+#include <sys/stat.h>
 #include <X11/Xlib.h>
 
-#define SHAREDFILE "/.vecratsharedfile"
+// Definitions
+#define SHMKEY 893422
+#define SHMSIZE 8
+#define INTSIZE 4
+#define CODEEXIT 111
 
 // Setup vars
 int quitRequest = 0;
-int vecArr[10] = {0, 1, 5, 5, 8, 10, 10, 20, 40, 50};
+int vecArr[10] = {0, 10, 5, 10, 10, 15, 15, 20, 20, 25, 25, 30, 35};
 int vecArrPos[2] = {0, 0};
-int vecArrOffsetArr[2] = {0, 0};
-void *vecArrOffset;
-char *sharedFileDir;
-void *mmapFileDescript;
-int fileDescript;
+int vecArrLen;
+int shmId;
+int *shmPntr;
 Display *XDisplay;
 Window XRootWin;
 
 int main() {
-  // Malloc space holding vector offset
-  vecArrOffset = malloc((sizeof(int)*2));
+  // Set vecArrLen
+  vecArrLen=sizeof(vecArr)/sizeof(int);
+  printf("%d\n", vecArrLen);
 
-  // Set pointer to vecArrOffsetArr;
-  vecArrOffset = &vecArrOffsetArr;
+  // Map pointer to memory w/ SHMKEY
+  if((shmId=shmget(SHMKEY, SHMSIZE, IPC_CREAT | 0770)) < 0){
+    printf("Unable to get memory space...\n");
+    perror("shmget");
+    return(0);
+  }
+  else if((shmPntr=shmat(shmId, NULL, IPC_CREAT | 0770)) < 0){
+    printf("Unable to get memory space...\n");
+    perror("shmmat");
+    return(0);
+  }
 
-  // Get shared file directory
-  sharedFileDir = strcat(getenv("HOME"), SHAREDFILE);
-
-  // Create a file descriptor and open file
-
-  // Map file to memory
+  // Zero out shmPntr
+  shmPntr[0]=0;
+  shmPntr[1]=0;
 
   // Setup for xwarppointer
-  Display *XDisplay = XOpenDisplay(0);
-  Window XRootWin = XRootWindow(XDisplay, 0);
+  XDisplay = XOpenDisplay(0);
+  XRootWin = XRootWindow(XDisplay, 0);
   XSelectInput(XDisplay, XRootWin, KeyReleaseMask);
 
   // Program loop
+  //  while(!quitRequest){
   while(!quitRequest){
-    // Get array offset from shared file
-    // Apply mouse velocity
-    // XWarpPointer(XDisplay, None, XRootWin, 0, 0, 0, 0, vecArr[0], vecArr[1]);
-    // XFlush(XDisplay);
-    if(getchar()=='q'){
-      quitRequest = 1;
+    printf("%d\n", shmPntr[0]);
+    printf("%d\n", shmPntr[1]);
+    // Check for codes
+    if(shmPntr[0]==CODEEXIT){
+      if(shmPntr[1]==CODEEXIT){
+        quitRequest=1;
+      }
     }
+    // Add shmPntr to vecArrPos
+    vecArrPos[0]=vecArrPos[0]+shmPntr[0];
+    vecArrPos[1]=vecArrPos[1]+shmPntr[1];
+    // Trim vecArrPos to bounds of vecArr
+    if(vecArrPos[0]<0){vecArrPos[0]=0;}
+    if(vecArrPos[1]<0){vecArrPos[1]=0;}
+    if(vecArrPos[0]>vecArrLen){vecArrPos[0]=vecArrLen;}
+    if(vecArrPos[1]>vecArrLen){vecArrPos[1]=vecArrLen;}
+    printf("%d\n", vecArrPos[0]);
+    printf("%d\n", vecArrPos[1]);
+    // Apply mouse velocity
+    XWarpPointer(XDisplay, None, None, 0, 0, 0, 0, vecArrPos[0], vecArrPos[1]);
+    XFlush(XDisplay);
+    // Zero out shmPntr
+    shmPntr[0]=0;
+    shmPntr[1]=0;
+    // Decrament vecArrPos
+    vecArrPos[0]--;
+    vecArrPos[1]--;
+    // Sleep/Pause
+    usleep(7500);
   }
 
-  // Close Pointers/file
-  close(fileDescript);
-  free(vecArrOffset);
-
+  // Exit
   return(0);
 }
